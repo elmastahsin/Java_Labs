73b411716b43152e89b4dafa8ff6b00773c34c64
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-10-10 23:00:02 +0300

    JavaLabs

---
+++ src/lab10_class_Object/scrumTask/Developer.java
@@ -0,0 +1,13 @@
+package lab10_class_Object.scrumTask;
+
+public class Developer {
+    private String name;
+    private int age;
+
+    private char gender;
+    private String id;
+    private String jobTitle;
+    private double hourlyRate;
+
+    private String programmingLanguage;
+}
