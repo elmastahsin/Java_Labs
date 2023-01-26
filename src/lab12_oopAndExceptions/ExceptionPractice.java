79a757f8c526d1e79e16d6e8935b4ee741491f6c
tyson717 <111126837+tyson717@users.noreply.github.com>
2023-01-26 12:33:21 +0300

    JavaLabs 12 Exceptions

--- src/lab12_oopAndExceptions/ExceptionPractice.java
+++ src/lab12_oopAndExceptions/ExceptionPractice.java
@@ -1,17 +1,46 @@
 package lab12_oopAndExceptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class ExceptionPractice {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         String str =null;
         try {
 
           String result=   str.repeat(3);
             System.out.println(result);
             System.out.println("try block");
-        }catch (NullPointerException e){
+        }catch (RuntimeException e){
             e.printStackTrace();
             System.out.println("catch block");
+            str ="Java";
+            String result = str.repeat(3);
+            System.out.println(result);
         }
 
+        System.out.println("-----------------------------");
+
+
+            new FileInputStream("src/lab12_oopAndExceptions/lab 12 class notes");
+
+    }
+    public void method1()throws FileNotFoundException{
+        new FileInputStream("src/lab12_oopAndExceptions/lab 12 class notes");
+
+    }
+    public void method2(){
+        //method1();
+    }
+    public void method3(){
+        try {
+            new FileInputStream("src/lab12_oopAndExceptions/lab 12 class notes");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public void method4(){
+        method3();
     }
 }
