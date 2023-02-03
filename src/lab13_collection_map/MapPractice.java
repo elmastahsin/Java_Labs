d88efc7c169c6346b2bd39cddc631264214bf4ad
tyson717 <111126837+tyson717@users.noreply.github.com>
2023-02-03 13:11:20 +0300

    JavaLabs 13 Collections

---
+++ src/lab13_collection_map/MapPractice.java
@@ -0,0 +1,32 @@
+package lab13_collection_map;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class MapPractice {
+    public static void main(String[] args) {
+
+
+        Map<String, Object> car1 = new LinkedHashMap<>();
+
+        car1.put("brand","BMW");
+        car1.put("model","X5");
+        car1.put("body","SUV");
+        car1.put("year",2021); // autoboxing
+        car1.put("auto",true); // autoboxing
+        car1.put("price",45000);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
