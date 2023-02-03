55cd102985f0add6d19cc940913867775cbb3077
tyson717 <111126837+tyson717@users.noreply.github.com>
2023-02-03 14:29:33 +0300

    JavaLabs 13 Collections

--- src/lab13_collection_map/MapPractice.java
+++ src/lab13_collection_map/MapPractice.java
@@ -10,22 +10,42 @@ public class MapPractice {
 
         Map<String, Object> car1 = new LinkedHashMap<>();
 
-        car1.put("brand","BMW");
-        car1.put("model","X5");
-        car1.put("body","SUV");
-        car1.put("year",2021); // autoboxing
-        car1.put("auto",true); // autoboxing
-        car1.put("price",45000);
+        car1.put("brand", "BMW");
+        car1.put("model", "X5");
+        car1.put("body", "SUV");
+        car1.put("year", 2021); // autoboxing
+        car1.put("auto", true); // autoboxing
+        car1.put("price", 45000);
 
+        System.out.println(car1.size());
 
+        System.out.println(car1);
 
+        car1.put("price", 55000);
 
+        System.out.println("--------------------------------------------");
 
+        System.out.println(car1);
 
+        System.out.println(car1.get("brand") + ":" + car1.get("model"));
 
+        System.out.println("--------------------------------------------");
 
+        for (String key : car1.keySet()) {
+            System.out.println(key);
+        }
+        System.out.println("--------------------------------------------");
+        for (Map.Entry<String, Object> entry : car1.entrySet()) {
+            //System.out.println(entry);
+         //   System.out.println(entry.getKey() + " : " + entry.getValue());
+            if (entry.getKey().equalsIgnoreCase("year")){
+                entry.setValue(2019);
+            }
 
-
+            if (entry.getKey().equalsIgnoreCase("price")){
+                entry.setValue(55000);
+            }
+        }
 
 
     }
